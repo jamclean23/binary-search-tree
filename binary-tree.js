0ad1e4c5a114430540d4073554e82f542259eeef
jamclean23 <jamclean23@gmail.com>
2022-12-30 14:25:16 -0500

    buildTree() pseudocode

--- binary-tree.js
+++ binary-tree.js
@@ -1,7 +1,8 @@
 // Node project exploring binary search tree operations
 
-// FUNCTIONS
 
+
+// FUNCTIONS----------------------------------------------------
 // NODE FACTORY
 // Inputs: value, left child, right child
 // Outputs: an object containing these arguments
@@ -18,8 +19,20 @@ function createNode(value = null, leftChild = null, rightChild = null) {
 // Inputs: an unsorted array
 // Outputs: an object containing a balanced binary tree
 
+function buildTree(array) {
+    // sort the list and remove any duplicates with a merge sort
+    // recursively build the tree
+        // find the middle element and assign it as the root
+        // left is the array up to the middle
+        // right is the array after the middle
+        // root.leftChild is recurse(left)
+        // root.rightChild is recurse(right)
+        // return the root
+    //return the result of the recursion
+}
 
-//INIT
+
+//INIT-----------------------------------------------------------
 
 const unsortedArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
