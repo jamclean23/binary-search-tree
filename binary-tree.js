3c7dc5c60d9176f6abb613327ed88ff4310fba3f
jamclean23 <jamclean23@gmail.com>
2022-12-30 13:32:10 -0500

    Add node factory

--- binary-tree.js
+++ binary-tree.js
@@ -1,1 +1,26 @@
 // Node project exploring binary search tree operations
+
+// FUNCTIONS
+
+// NODE FACTORY
+// Inputs: value, left child, right child
+// Outputs: an object containing these arguments
+
+function createNode(value = null, leftChild = null, rightChild = null) {
+    return {
+        value: value,
+        leftChild: leftChild,
+        rightChild: rightChild
+    }
+}
+
+// TREE FACTORY
+// Inputs: an unsorted array
+// Outputs: an object containing a balanced binary tree
+
+
+//INIT
+
+const unsortedArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+// call buildTree() with array
