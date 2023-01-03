4d622c524c8ea7c6d6968cdee3afaebe61546dfb
jamclean23 <jamclean23@gmail.com>
2023-01-03 11:35:48 -0500

    Add merge sort and build a tree. Add testing comments

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ OBJECTIVES:
 Assignment
 You’ll build a balanced BST in this assignment. Do not use duplicate values because they make it more complicated and result in trees that are much harder to balance. Therefore, be sure to always remove duplicate values or check for an existing value before inserting.
 
-Build a Node class / factory. It should have an attribute for the data it stores as well as its left and right children.
+✓Build a Node class / factory. It should have an attribute for the data it stores as well as its left and right children.
 
-Build a Tree class / factory which accepts an array when initialized. The Tree class should have a root attribute which uses the return value of buildTree which you’ll write next.
+✓Build a Tree class / factory which accepts an array when initialized. The Tree class should have a root attribute which uses the return value of buildTree which you’ll write next.
 
-Write a buildTree function which takes an array of data (e.g. [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]) and turns it into a balanced binary tree full of Node objects appropriately placed (don’t forget to sort and remove duplicates!). The buildTree function should return the level-0 root node.
+✓Write a buildTree function which takes an array of data (e.g. [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]) and turns it into a balanced binary tree full of Node objects appropriately placed (don’t forget to sort and remove duplicates!). The buildTree function should return the level-0 root node.
 
 Tip: If you would like to visualize your binary search tree, here is a prettyPrint() function that will console.log your tree in a structured format. This function will expect to receive the root of your tree as the value for the node parameter.
 
--- binary-tree.js
+++ binary-tree.js
@@ -3,37 +3,119 @@
 
 
 // FUNCTIONS----------------------------------------------------
+
+
+
 // NODE FACTORY
 // Inputs: value, left child, right child
 // Outputs: an object containing these arguments
 
-function createNode(value = null, leftChild = null, rightChild = null) {
+function createNode(value = null, left = null, right = null) {
     return {
         value: value,
-        leftChild: leftChild,
-        rightChild: rightChild
+        left: left,
+        right: right
     }
 }
+
+
 
 // TREE FACTORY
 // Inputs: an unsorted array
 // Outputs: an object containing a balanced binary tree
+function createTree(array) {
+    let tree = {};
+    tree.root = buildTree(array);
+    return tree;
 
-function buildTree(array) {
-    // sort the list and remove any duplicates with a merge sort
-    // recursively build the tree
-        // find the middle element and assign it as the root
-        // left is the array up to the middle
-        // right is the array after the middle
-        // root.leftChild is recurse(left)
-        // root.rightChild is recurse(right)
-        // return the root
-    //return the result of the recursion
+    // LOCALLY SCOPED FUNCTIONS
+
+    function buildTree(array) {
+
+        // sort the list and remove any duplicates with a merge sort
+        let sortedArray = mergeSort(array);
+    
+        // recursively build the tree
+        return sortedArrayToBalancedTree(sortedArray);
+    
+    
+    
+        //LOCALLY SCOPED FUNCTIONS
+    
+        function sortedArrayToBalancedTree (sortedArray) {
+            if (sortedArray.length === 1) {
+                let root = createNode(sortedArray[0]);
+                return root;
+            }
+    
+            let middle = Math.floor(sortedArray.length / 2);
+            let left = sortedArray.slice(0, middle);
+            let right = sortedArray.slice(middle);
+    
+            let root = createNode(sortedArray[middle], sortedArrayToBalancedTree(left), 
+                sortedArrayToBalancedTree(right));
+            
+            return root;
+        }
+    
+        function mergeSort (array) {
+            if (array.length <= 1) {
+                return array;
+            }
+    
+            let middle = Math.floor(array.length / 2);
+            let left = array.slice(0, middle);
+            let right = array.slice(middle);
+    
+            return merge(mergeSort(left), mergeSort(right));
+    
+            function merge (left, right, tempArray = []) {
+                while(left.length > 0 && right.length > 0) {
+                    if (left[0] < right[0]) {
+                        tempArray.push(left.shift());
+                    } else if (right[0] < left[0]) {
+                        tempArray.push(right.shift());
+                    } else if (right[0] === left[0]){
+                        tempArray.push(left.shift());
+                        right.shift();
+                    }
+                }
+                return [...tempArray, ...left, ...right];
+            }
+        }
+    }
 }
+
 
 
 //INIT-----------------------------------------------------------
 
 const unsortedArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
-// call buildTree() with array
+let tree = createTree(unsortedArray);
+console.log('BINARY TREE: ');
+console.dir(tree, { depth: null });
+
+//TESTING-------------------------------------------------------
+T
+// Insert Value
+
+// Delete Value
+
+// Find value
+
+// Breadth-first operations
+
+// In-order operations
+
+// Pre-order operations
+
+// Post-order operations
+
+// Height function
+
+// Depth function
+
+// isBalanced() check if the tree is balanced
+
+// rebalance() balances a tree 
